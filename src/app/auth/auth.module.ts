e1c7c91a71fdae4572913e1e809f7721f6c191bd
Maycon Jardim <mayconsmill@gmail.com>
2022-08-30 20:16:34 -0300

    Register component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import {NgModule} from '@angular/core'
+import {CommonModule} from '@angular/common';
+import { RegisterComponent } from './components/register/register/register.component'
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    RegisterComponent
+  ],
+  imports: [CommonModule],
 })
-export class AuthModule { }
+export class AuthModule {}
