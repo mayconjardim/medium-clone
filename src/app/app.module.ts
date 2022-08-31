92f19ad2e8c242a69e7684b582d27d42c1004d04
Maycon Jardim <mayconsmill@gmail.com>
2022-08-30 21:16:09 -0300

    register actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import {AuthModule} from './auth/auth.module'
 import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
+import {StoreModule} from '@ngrx/store'
 
 import {AppRoutingModule} from './app-routing.module'
-import {AppComponent} from './app.component';
-import { StoreModule } from '@ngrx/store'
+import {AppComponent} from './app.component'
+import {AuthModule} from './auth/auth.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, StoreModule.forRoot({}, {})],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    StoreModule.forRoot({}, {}),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core'
-import {FormGroup, FormBuilder, Validators} from '@angular/forms'
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+import {Store} from '@ngrx/store'
+
+import {registerAction} from './../../store/actions'
 
 @Component({
   selector: 'mc-register',
@@ -9,7 +12,7 @@ import {FormGroup, FormBuilder, Validators} from '@angular/forms'
 export class RegisterComponent implements OnInit {
   form: FormGroup
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm()
@@ -24,6 +27,6 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit() {
-    this.form.valid
+    this.store.dispatch(registerAction(this.form.value))
   }
 }
